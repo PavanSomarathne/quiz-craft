732d162b756391f33731e1a3998f3bc7dd6db341
Pavan Somarathne <pavan.somarathne@axiatadigitallabs.com>
2024-11-10 00:52:06 +0530

    Update app.js

--- App.js
+++ App.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import { registerRootComponent } from 'expo';
-import Index from './app/index'; // Assuming index.tsx is your main entry point in the app folder
-
-export default function App() {
-  return <Index />;
-}
-
-// Register the main component as the root component
-registerRootComponent(App);
+import 'expo-router/entry';
